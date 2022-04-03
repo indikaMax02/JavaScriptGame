334286b0c215687fc5b4454de5c57bb3122856bf
indikaMax02 <indikamax02@gmail.com>
2022-04-03 08:47:07 +0530

    apple draw and eat apple

--- js/index.js
+++ js/index.js
@@ -4,9 +4,12 @@ const ctx=canvas.getContext('2d');
 let speed=7;
 
 let tileCount=20;
-let tileSize=canvas.width / tileCount-2;
+let tileSize=canvas.width / tileCount;
 let headX= 10;
 let headY= 10;
+
+let appleX=5;
+let appleY=5;
 
 let xVelocity=0;
 let yVelocity=0;
@@ -15,6 +18,8 @@ let yVelocity=0;
 function drawGame() {
        clearScreen();
        changeSnakePosition();
+       checkAppleCollection();
+       drawApple();
        drawSnake();
        setTimeout(drawGame,1000/ speed);
 }
@@ -36,6 +41,17 @@ function drawSnake() {
     ctx.fillRect(headX*tileCount, headY*tileCount,tileSize,tileSize);
 }
 
+function checkAppleCollection() {
+     if (appleX == headX & appleY==headY){
+         appleX=Math.floor(Math.random() * tileCount);
+         appleY=Math.floor(Math.random() * tileCount);
+     }
+}
+
+function drawApple(){
+    ctx.fillStyle='red';
+    ctx.fillRect(appleX*tileCount,appleY*tileCount,tileSize,tileSize);
+}
 
 document.body.addEventListener('keydown',function (event) {
 
@@ -43,24 +59,36 @@ document.body.addEventListener('keydown',function (event) {
 
     //up
    if (event.key=="ArrowUp"){
+       if (yVelocity==1){
+           return;
+       }
        yVelocity=-1;
        xVelocity=0;
    }
 
    //down
     if (event.key=="ArrowDown"){
+        if (yVelocity==-1){
+            return;
+        }
         yVelocity=1;
         xVelocity=0;
     }
 
     //left
     if (event.key=="ArrowLeft"){
+        if (xVelocity==1){
+            return;
+        }
         yVelocity=0;
         xVelocity=-1;
     }
 
     //right
     if (event.key=="ArrowRight"){
+        if (xVelocity==-1){
+            return;
+        }
         yVelocity=0;
         xVelocity=1;
     }
