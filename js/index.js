bf013480da55b0804b9de3237aff3386f6753791
indikaMax02 <indikamax02@gmail.com>
2022-04-03 08:04:59 +0530

    snake position and add KeyController

--- js/index.js
+++ js/index.js
@@ -8,9 +8,13 @@ let tileSize=canvas.width / tileCount-2;
 let headX= 10;
 let headY= 10;
 
+let xVelocity=0;
+let yVelocity=0;
+
 //game loop
 function drawGame() {
        clearScreen();
+       changeSnakePosition();
        drawSnake();
        setTimeout(drawGame,1000/ speed);
 }
@@ -20,9 +24,49 @@ function clearScreen() {
      ctx.fillRect(0,0,canvas.width,canvas.height);
 }
 
+
+function changeSnakePosition() {
+       headX=headX + xVelocity;
+       headY=headY + yVelocity;
+}
+
+
 function drawSnake() {
         ctx.fillStyle='green'
     ctx.fillRect(headX*tileCount, headY*tileCount,tileSize,tileSize);
 }
+
+
+document.body.addEventListener('keydown',function (event) {
+
+    console.log(event.key);
+
+    //up
+   if (event.key=="ArrowUp"){
+       yVelocity=-1;
+       xVelocity=0;
+   }
+
+   //down
+    if (event.key=="ArrowDown"){
+        yVelocity=1;
+        xVelocity=0;
+    }
+
+    //left
+    if (event.key=="ArrowLeft"){
+        yVelocity=0;
+        xVelocity=-1;
+    }
+
+    //right
+    if (event.key=="ArrowRight"){
+        yVelocity=0;
+        xVelocity=1;
+    }
+
+
+});
+
 
 drawGame();
